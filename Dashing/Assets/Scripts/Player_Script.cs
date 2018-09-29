ffff17c48c136a8bb15164283ecc57b299e98758
David Mincer <david.r.mincer@live.com>
2018-09-29 18:05:15 +0100

    Dash WORKING
    Still unable to set direction

---
+++ Dashing/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    public GameObject directionalArrow,
+        directionPivot;
+    public float dashForce;
+
+    private Vector3 direction;
+    private Rigidbody rb;
+    private bool moving;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moving = false;
+
+        Dash();
+    }
+
+    private void FixedUpdate()
+    {
+        moving = !((Mathf.Abs(rb.velocity.x) < 0.001) &&
+            (Mathf.Abs(rb.velocity.y) < 0.001) &&
+            (Mathf.Abs(rb.velocity.z) < 0.001));
+    }
+
+    private void LateUpdate()
+    {
+        if (moving)
+        {
+            directionalArrow.SetActive(false);
+        }
+        else
+        {
+            directionalArrow.SetActive(true);
+        }
+    }
+
+    public void Dash()
+    {
+        direction = directionalArrow.transform.position - transform.position;
+        direction.y = 0.0f;
+
+        rb.AddForce(direction * dashForce);
+    }
+}
