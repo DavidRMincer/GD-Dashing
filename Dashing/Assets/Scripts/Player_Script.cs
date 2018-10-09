0f979c319ff0ab9fdb0f61365c2d6dda3f345e8f
DavidRMincer <33055698+DavidRMincer@users.noreply.github.com>
2018-10-09 17:00:24 +0100

    Camera Follow with Delay

--- Dashing/Assets/Scripts/Player_Script.cs
+++ Dashing/Assets/Scripts/Player_Script.cs
@@ -6,32 +6,36 @@ public class Player_Script : MonoBehaviour
 {
     public GameObject directionalArrow,
         directionPivot;
+    public Camera followingCamera;
     public float dashForce,
-        rotationSpeed;
+        rotationSpeed,
+        cameraSpeed;
 
     private Vector3 direction;
     private Rigidbody rb;
     private bool moving;
+    private Vector3 cameraOffset;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         moving = false;
-
-        //Dash();
+        cameraOffset = followingCamera.transform.position - transform.position;
     }
 
     private void FixedUpdate()
     {
-        moving = !((Mathf.Abs(rb.velocity.x) < 0.001) &&
-            (Mathf.Abs(rb.velocity.y) < 0.001) &&
-            (Mathf.Abs(rb.velocity.z) < 0.001));
+        moving = !((Mathf.Abs(rb.velocity.x) < 0.1) &&
+            (Mathf.Abs(rb.velocity.y) < 0.1) &&
+            (Mathf.Abs(rb.velocity.z) < 0.1));
 
         if (!moving) GetInput();
     }
 
     private void LateUpdate()
     {
+        updateCameraPosition();
+
         if (moving)
         {
             directionalArrow.SetActive(false);
@@ -40,6 +44,14 @@ public class Player_Script : MonoBehaviour
         {
             directionalArrow.SetActive(true);
         }
+    }
+
+    private void updateCameraPosition()
+    {
+        Vector3 newCamPos = transform.position + cameraOffset;
+        Vector3 camDirection = (newCamPos - followingCamera.transform.position) * Time.deltaTime * cameraSpeed;
+
+        followingCamera.transform.position += camDirection;
     }
 
     private void GetInput()
