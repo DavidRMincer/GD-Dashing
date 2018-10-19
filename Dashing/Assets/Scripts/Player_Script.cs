8c5448a1bf32bbaaab2c0f80d304e291d570eb9b
DavidRMincer <33055698+DavidRMincer@users.noreply.github.com>
2018-10-19 17:12:07 +0100

    Final Physics & Levels Pull (IGNORE PREVIOUS ONES)

--- Dashing/Assets/Scripts/Player_Script.cs
+++ Dashing/Assets/Scripts/Player_Script.cs
@@ -6,32 +6,39 @@ public class Player_Script : MonoBehaviour
 {
     public GameObject directionalArrow,
         directionPivot;
+    public Camera followingCamera;
+    public ParticleSystem dust;
     public float dashForce,
-        rotationSpeed;
+        rotationSpeed,
+        cameraSpeed,
+		cameraShakeDuration,
+		cameraShakeIntensity;
 
     private Vector3 direction;
     private Rigidbody rb;
     private bool moving;
+    private Vector3 cameraOffset;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         moving = false;
-
-        //Dash();
+        cameraOffset = followingCamera.transform.position - transform.position;
     }
 
     private void FixedUpdate()
     {
-        moving = !((Mathf.Abs(rb.velocity.x) < 0.001) &&
-            (Mathf.Abs(rb.velocity.y) < 0.001) &&
-            (Mathf.Abs(rb.velocity.z) < 0.001));
+        moving = !((Mathf.Abs(rb.velocity.x) < 0.1) &&
+            (Mathf.Abs(rb.velocity.y) < 0.1) &&
+            (Mathf.Abs(rb.velocity.z) < 0.1));
 
         if (!moving) GetInput();
     }
 
     private void LateUpdate()
     {
+        updateCameraPosition();
+
         if (moving)
         {
             directionalArrow.SetActive(false);
@@ -40,6 +47,14 @@ public class Player_Script : MonoBehaviour
         {
             directionalArrow.SetActive(true);
         }
+    }
+
+    private void updateCameraPosition()
+    {
+        Vector3 newCamPos = transform.position + cameraOffset;
+        Vector3 camDirection = (newCamPos - followingCamera.transform.position) * Time.deltaTime * cameraSpeed;
+
+        followingCamera.transform.position += camDirection;
     }
 
     private void GetInput()
@@ -59,5 +74,28 @@ public class Player_Script : MonoBehaviour
         direction.y = 0.0f;
 
         rb.AddForce(direction * dashForce);
+
+        dust.Play();
+		
+		StartCoroutine(ShakeCamera());
     }
+	
+	public IEnumerator ShakeCamera()
+	{
+		Vector3 originalPos = followingCamera.transform.localPosition;
+		
+		float elapsed = 0.0f;
+		
+		while (elapsed < cameraShakeDuration)
+		{
+			float x = followingCamera.transform.position.x + (Random.Range(-1.0f, 1.0f) * cameraShakeIntensity);
+			float y = followingCamera.transform.position.y + (Random.Range(-1.0f, 1.0f) * cameraShakeIntensity);
+			
+			followingCamera.transform.localPosition = new Vector3(x, y, originalPos.z);
+			
+			elapsed += Time.deltaTime;
+			
+			yield return null;
+		}
+	}
 }
