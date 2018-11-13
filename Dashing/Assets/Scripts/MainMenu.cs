769763dfe5425efe68bfec71668327017b8ff1b0
David Mincer <david.r.mincer@live.com>
2018-11-13 12:43:37 +0000

    Main Menu Implemented

---
+++ Dashing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenue : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Edit this to change the first played the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
