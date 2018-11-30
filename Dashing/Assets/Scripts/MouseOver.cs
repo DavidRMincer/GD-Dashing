0669f79436e2b71c5a6ddd6b00d7eea964d6ef21
DavidRMincer <33055698+DavidRMincer@users.noreply.github.com>
2018-11-30 15:01:27 +0000

    Menu Fixed

---
+++ Dashing/Assets/Scripts/MouseOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseOver : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler {
+
+    public Color onMouseOverColor;
+
+    private TextMeshProUGUI text;
+    private Color startColor;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = onMouseOverColor;
+        text.fontSize = 38;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = startColor;
+        text.fontSize = 36;
+    }
+
+    // Use this for initialization
+    void Start () {
+        text = GetComponent<TextMeshProUGUI>();
+        startColor = text.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
